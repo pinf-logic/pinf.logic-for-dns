7f8539f94c1e53052c492b92012effb3177c9c8c
Christoph Dorn <christoph@christophdorn.com>
2015-05-12 15:08:05 -0700

    wip

--- plugin.js
+++ plugin.js
@@ -8,8 +8,22 @@ exports.for = function (API) {
 
 		// TODO: Validate input config. Complete missing data. Generate uniform output config.
 
+		var validatedConfig = {
+			records: {}
+		};
+
+		for (var name in config.records) {
+			if (config.records[name].domain) {
+				config.records[name].domain = config.records[name].domain.toLowerCase();
+			}
+			if (config.records[name].data) {
+				config.records[name].data = config.records[name].data.toLowerCase();
+			}
+			validatedConfig.records[name.toLowerCase()] = config.records[name];
+		}
+
 		return {
-			"pinf.logic-for-dns~0": config
+			"pinf.logic-for-dns~0": validatedConfig
 		};
 	}
 
